e69a18e83de64aede9eae4bb1f20d1200d1bf824
hemanth26-web <sonnailahemanth@gmail.com>
2026-01-29 07:05:26 +0530

    Added portfolio structure with Html, css, js,whats image

---
+++ script.js
@@ -0,0 +1,26 @@
+// toggle icon navbar
+
+// scroll section
+let sections = document.querySelectorAll('section');
+let navlink = document.querySelectorAll('heading nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec=> {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            // actibe navbar links
+            navlinks.forEach(links=> {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*#' + id + ']').classList.add('active');
+            });
+        }
+    });
+        
+    // sticky header 
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY>100);
+}
